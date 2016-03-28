9eedec391ecb20a5cf71b0ddf60bbc5d3cd5ebc1
Jan Rabe <cloudgazer3d@gmail.com>
2016-03-28 12:23:57 +0200

    added selection controls

---
+++ UnityClient/Assets/Scripts/Controls/OvrInput.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.Controls
+{
+  public class OvrInput : MonoBehaviour
+  {
+
+    [SerializeField] TextMesh _text;
+
+    void Start()
+    {
+      OVRTouchpad.Create();
+      OVRTouchpad.TouchHandler += HandleTouchHandler;
+    }
+
+    private void HandleTouchHandler(object sender, EventArgs e)
+    {
+      var touchArgs = (OVRTouchpad.TouchArgs) e;
+      if (touchArgs.TouchType == OVRTouchpad.TouchEvent.SingleTap)
+      {
+        //TODO: Insert code here to handle a single tap.  Note that there are other TouchTypes you can check for like directional swipes, but double tap is not currently implemented I believe.
+        Debug.Log("Received SingleTap");
+        _text.text = "singletap";
+      }
+      else
+      {
+        Debug.Log("Received " + e);
+        _text.text = touchArgs.TouchType.ToString();
+      }
+    }
+  }
+}
---
+++ UnityClient/Assets/Scripts/Game/SelectionController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.Game
+{
+  public class SelectionController : MonoBehaviour
+  {
+    [SerializeField] readonly List<GameObject> _selected = new List<GameObject>();
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+
+      if ( Input.GetMouseButtonDown (0)){
+         RaycastHit hit;
+         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+         if ( Physics.Raycast (ray,out hit,100.0f)) {
+           StartCoroutine(ScaleMe(hit.transform.parent));
+           Debug.Log("You selected the " + hit.transform.parent.name);
+         }
+       }
+
+
+    }
+
+    IEnumerator ScaleMe(Transform objTr) {
+        objTr.localScale *= 1.2f;
+        yield return new WaitForSeconds(0.5f);
+        objTr.localScale /= 1.2f;
+    }
+  }
+}
