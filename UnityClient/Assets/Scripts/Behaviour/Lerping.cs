8e24eddef49f2ab266a5e7153a38cd1aa4968e33
Jan Rabe <cloudgazer3d@gmail.com>
2016-03-30 14:55:39 +0200

    lerped a cube

---
+++ UnityClient/Assets/Scripts/Behaviour/Lerping.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+namespace Assets.Scripts.Behaviour
+{
+  public class Lerping : NetworkBehaviour
+  {
+    [SyncVar] [SerializeField] private Vector3 _syncPosition;
+
+    [SyncVar] private float _lerpRate = 15;
+
+    void FixedUpdate()
+    {
+      CmdProvidePositionToServer(transform.position);
+      LerpPosition();
+    }
+
+    void LerpPosition()
+    {
+      if (!isLocalPlayer)
+      {
+        // the 10 is a magic number. you probably want to set some sort of movement rate.
+        this.transform.position = Vector3.Lerp(this.transform.position, _syncPosition, Time.deltaTime*_lerpRate);
+      }
+    }
+
+    [Command]
+    void CmdProvidePositionToServer(Vector3 position)
+    {
+      _syncPosition = position;
+    }
+  }
+}
